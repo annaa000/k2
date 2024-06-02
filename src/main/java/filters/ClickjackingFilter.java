1b360b156ef3a0d836a1b0ffaf5f63f42e0c780c
Anna <a.atte2@studenti.unisa.it>
2024-06-02 19:46:52 +0200

    id-5 resolved

---
+++ src/main/java/filters/ClickjackingFilter.java
@@ -0,0 +1,28 @@
+package filters;
+
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class ClickjackingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("X-Frame-Options", "DENY"); // O "SAMEORIGIN"
+        chain.doFilter(request, response);
+    }
+
+}
---
+++ src/main/java/filters/ContentSecurityPolicyFilter.java
@@ -0,0 +1,28 @@
+package filters;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class ContentSecurityPolicyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization code
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Content-Security-Policy", "frame-ancestors 'self'");
+        chain.doFilter(request, response);
+    }
+
+   
+}
